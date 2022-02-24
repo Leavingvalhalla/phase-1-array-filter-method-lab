8de5227d65d6dd1bb9d8b7a69226a3999d5512a5
Zach Lefler <leaving.valhalla@gmail.com>
2022-02-24 09:33:41 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(array, string) {
+  const drivers = array.filter(
+    (driver) => driver.toLowerCase() === string.toLowerCase()
+  );
+  return drivers;
+}
+
+function fuzzyMatch(array, string) {
+  const drivers = array.filter((driver) => driver.startsWith(string));
+  return drivers;
+}
+
+function matchName(array, string) {
+  const match = array.filter((driver) => driver.name === string);
+  return match;
+}
